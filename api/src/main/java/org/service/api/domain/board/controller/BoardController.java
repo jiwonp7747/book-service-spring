4a550627e25f1366b9ce6d846a9553ab515d7d15
jiwon <angry9908@gmail.com>
2024-08-09 17:28:07 +0900

    BoardController

---
+++ api/src/main/java/org/service/api/domain/board/controller/BoardController.java
@@ -0,0 +1,11 @@
+package org.service.api.domain.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/board")
+public class BoardController {
+}
